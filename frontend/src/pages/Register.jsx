e9b8d605fd9f285b521cdf263050041824775915
anni <asthakur285@gmail.com>
2026-07-10 18:45:25 +0530

    removed / after API_url

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -18,7 +18,7 @@ const Register = () => {
     e.preventDefault();
     setLoading(true);
     try {
-      const res = await fetch(`${API_URL}/api/auth/otp-verification`, {
+      const res = await fetch(`${API_URL}api/auth/otp-verification`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
