ea949f68ce1adaa8b690a558a13822f0646bbaa7
anni <asthakur285@gmail.com>
2026-07-10 08:47:37 +0530

    backend index.js revert back

--- backend/index.js
+++ backend/index.js
@@ -1,49 +1,35 @@
-// const express = require("express");
-// const cors = require("cors");
-// const connectDB = require("./config/db");
-// const dotenv = require("dotenv");
-// dotenv.config();
-// connectDB();
-
-// const userRoutes = require("./routes/authRoutes");
-
-// const app = express();
-// app.use(cors(
-//     {
-//         origin: ['http://localhost:3000', 'http://127.0.0.1:3000', process.env.FRONTEND_URL],
-//         credentials: true
-//     }
-// ));
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.get("/", (req, res) => {
-//     res.send("shopNest backend is running");
-// });
-
-// app.use('/api/auth', userRoutes)
-// app.use('/api/products', require('./routes/productRoutes'));
-// app.use('/api/orders', require('./routes/orderRoutes'));
-// app.use('/api/payment', require('./routes/paymentRoutes'));
-// app.use('/api/analytics', require('./routes/analyticsRoutes'));
-
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () =>{
-//     console.log(`Server is running on port ${PORT}`);
-// });
-
 const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+dotenv.config();
+connectDB();
+
+const userRoutes = require("./routes/authRoutes");
 
 const app = express();
+app.use(cors(
+    {
+        origin: ['http://localhost:3000', 'http://127.0.0.1:3000', process.env.FRONTEND_URL],
+        credentials: true
+    }
+));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("Backend is alive");
+    res.send("shopNest backend is running");
 });
 
-const PORT = process.env.PORT || 5000;
+app.use('/api/auth', userRoutes)
+app.use('/api/products', require('./routes/productRoutes'));
+app.use('/api/orders', require('./routes/orderRoutes'));
+app.use('/api/payment', require('./routes/paymentRoutes'));
+app.use('/api/analytics', require('./routes/analyticsRoutes'));
 
-app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
 });
